783e8841b5dd9ef14eaafa2e29d261cdb1019537
AleksanderOkrasa <olekokrasa@gmail.com>
2023-03-12 12:34:01 +0100

    addition support for checking choice input data and created Exception BadChoiceInput

--- game_control.py
+++ game_control.py
@@ -13,11 +13,22 @@ class GameInput(ABC):
     def choice_input(self, choice = None):
         pass
 
+    def check_choice_input(self, choice):
+        if not self.is_good_choice_input():
+            print(f'Bad input [ {choice} ]; try again')
+            raise(BadChoiceInput(f'enter value: {choice}'))
+
+    def is_good_choice_input(self):
+        return choice.isdigit() and int(choice) in range(1,4)
+    
 class UserInput(GameInput):
     def choice_input(self, choice = None):
         print('Type a choice [1 - rock, 2 - paper, 3 - scissors]')
         choice = getch().decode('utf-8')
         return choice
     
+class BadChoiceInput(Exception):
+    pass
 user = UserInput('Player')
-print(user.choice_input())
+choice = user.choice_input()
+print(user.check_choice_input(choice))
