aed1d604b7510badf0a8c985e414bbb77a524024
AleksanderOkrasa <olekokrasa@gmail.com>
2023-03-12 12:22:57 +0100

    created a class user input inheriting from game_input and creation of input capture functionality

--- game_control.py
+++ game_control.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractclassmethod
 
+from msvcrt import getch
+
 class GameInput(ABC):
     def __init__(self, player):
         self.player = player
@@ -8,6 +10,14 @@ class GameInput(ABC):
         return self.player
     
     @abstractclassmethod
-    def input(self, choice = None):
+    def choice_input(self, choice = None):
         pass
 
+class UserInput(GameInput):
+    def choice_input(self, choice = None):
+        print('Type a choice [1 - rock, 2 - paper, 3 - scissors]')
+        choice = getch().decode('utf-8')
+        return choice
+    
+user = UserInput('Player')
+print(user.choice_input())
